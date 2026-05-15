0c173408c70565e8e8a504adda1436cadfb899cf
Luiz Guilherme C. da Silva <luiz.developer.acc@gmail.com>
2026-05-15 20:28:16 -0300

    feat: modified thread quantity

--- stress.py
+++ stress.py
@@ -214,7 +214,7 @@ def main():
 
     env = sys.argv[1]
     base_url = get_base_url(env)
-    num_threads = os.cpu_count()
+    num_threads = int(os.cpu_count() / 4)
 
     print(f"\nAmbiente : {env}")
     print(f"Endereço : {base_url}")
